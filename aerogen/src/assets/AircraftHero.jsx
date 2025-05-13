568f8161c1626cdb34c791feef6733a153401373
ECSze <emerson.sze.cics@ust.edu.ph>
2025-05-13 17:51:51 +0800

    Overwriting, You may proceed to Back End

--- aerogen/src/assets/AircraftHero.jsx
+++ aerogen/src/assets/AircraftHero.jsx
@@ -5,103 +5,115 @@ const aircrafts = [
   {
     name: "Airbus A220-100",
     image: "/A220-100.jpg",
-    range: "4,750 NM",
-    seats: 14,
-    runway: "5,360 ft",
-    lease: "$58,000 / month"
+    range: "3,750 NM",
+    seats: 140,
+    runway: "4,800 ft",
+    lease: "$5,000 / month",
+    available: 5
   },
   {
     name: "A220-300",
     image: "/A220-300.jpg",
-    range: "7,000 NM",
-    seats: 18,
-    runway: "6,000 ft",
-    lease: "$75,000 / month"
+    range: "4,500 NM",
+    seats: 140,
+    runway: "5,000 ft",
+    lease: "$6,000 / month",
+    available:5
   },
   {
     name: "A319NEO",
     image: "/A319neo.jpg",
-    range: "2,000 NM",
-    seats: 9,
-    runway: "3,200 ft",
-    lease: "$25,000 / month"
+    range: "3,700 NM",
+    seats: 156,
+    runway: "6,562 ft",
+    lease: "$7,000 / month",
+    available:5
   },
   {
     name: "A320NEO",
     image: "/A320neo.jpg",
-    range: "2,000 NM",
-    seats: 9,
-    runway: "3,200 ft",
-    lease: "$25,000 / month"
+    range: "4,230 NM",
+    seats: 180,
+    runway: "5,800 ft",
+    lease: "$10,000 / month",
+    available:5
   },
   {
     name: "A321NEO",
     image: "/A321neo.jpg",
-    range: "2,000 NM",
-    seats: 9,
-    runway: "3,200 ft",
-    lease: "$25,000 / month"
+    range: "3,800 NM",
+    seats: 220,
+    runway: "7,250 ft",
+    lease: "$15,000 / month",
+    available:5
   },
  
   {
     name: "A330NEO",
     image: "/A330neo.jpg",
-    range: "2,000 NM",
-    seats: 9,
-    runway: "3,200 ft",
-    lease: "$25,000 / month"
+    range: "6,000 NM",
+    seats: 320,
+    runway: "11,200 ft",
+    lease: "$25,000 / month",
+    available:5
   },
   {
     name: "A350-900",
     image: "/A350-900.jpg",
-    range: "2,000 NM",
-    seats: 9,
-    runway: "3,200 ft",
-    lease: "$25,000 / month"
+    range: "8,000 NM",
+    seats: 440,
+    runway: "13,200 ft",
+    lease: "$45,000 / month",
+    available:5
   },
   {
     name: "A350-1000",
     image: "/A350-1000.jpg",
-    range: "2,000 NM",
-    seats: 9,
-    runway: "3,200 ft",
-    lease: "$25,000 / month"
+    range: "8,900 NM",
+    seats: 520,
+    runway: "13,200 ft",
+    lease: "$75,000 / month",
+    available:5
   },
 
   {
     name: "BOEING 737 MAX 7",
     image: "/B737-7.jpg",
     range: "4,750 NM",
-    seats: 14,
+    seats: 140,
     runway: "5,360 ft",
-    lease: "$58,000 / month"
+    lease: "$12,000 / month",
+    available:5
   },
   
   {
     name: "BOEING 737 MAX 8",
     image: "/B737-8.jpg",
     range: "4,750 NM",
-    seats: 14,
-    runway: "5,360 ft",
-    lease: "$58,000 / month"
+    seats: 180,
+    runway: "6,360 ft",
+    lease: "$18,000 / month",
+    available:5
   },
 
   {
     name: "BOEING 737 MAX 9",
     image: "/B737-9.jpeg",
-    range: "4,750 NM",
-    seats: 14,
-    runway: "5,360 ft",
-    lease: "$58,000 / month"
+    range: "3,750 NM",
+    seats: 220,
+    runway: "7,360 ft",
+    lease: "$28,000 / month",
+    available:5
   },
 
   {
     name: "BOEING 737 MAX 10 ",
     image: "/B737-10.jpg",
-    range: "4,750 NM",
-    seats: 14,
+    range: "5,750 NM",
+    seats: 250,
     runway: "5,360 ft",
-    lease: "$58,000 / month"
+    lease: "$38,000 / month",
+    available:5
   },
 
   
@@ -109,10 +121,11 @@ const aircrafts = [
   {
     name: "BOEING 777-8",
     image: "/B777-8.jpg",
-    range: "4,750 NM",
-    seats: 14,
+    range: "8,750 NM",
+    seats: 520,
     runway: "5,360 ft",
-    lease: "$58,000 / month"
+    lease: "$108,000 / month",
+    available:5
   },
 
  
@@ -120,28 +133,31 @@ const aircrafts = [
   {
     name: "BOEING 787-8",
     image: "/B787-8.jpg",
-    range: "4,750 NM",
-    seats: 14,
-    runway: "5,360 ft",
-    lease: "$58,000 / month"
+    range: "7,750 NM",
+    seats: 320,
+    runway: "15,360 ft",
+    lease: "$48,000 / month",
+    available:5
   },
 
   {
     name: "BOEING 787-9",
     image: "/B787-9.jpg",
-    range: "4,750 NM",
-    seats: 14,
+    range: "8,750 NM",
+    seats: 380,
     runway: "5,360 ft",
-    lease: "$58,000 / month"
+    lease: "$58,000 / month",
+    available:5
   },
 
   {
     name: "BOEING 787-10",
     image: "/B787-10.jpg",
-    range: "4,750 NM",
-    seats: 14,
-    runway: "5,360 ft",
-    lease: "$58,000 / month"
+    range: "7,750 NM",
+    seats: 540,
+    runway: "7,380 ft",
+    lease: "$88,000 / month",
+    available:5
   }
 ];
 
@@ -159,6 +175,7 @@ const AircraftHero = () => {
             <p><strong>Range:</strong> {aircraft.range}</p>
             <p><strong>Seats:</strong> {aircraft.seats}</p>
             <p><strong>Min Runway:</strong> {aircraft.runway}</p>
+            <p><strong>Number of Planes Available:</strong> {aircraft.available}</p>
             <p><strong>Lease:</strong> {aircraft.lease}</p>
           </div>
           <button className="select-button">Select Aircraft</button>
