e96331736568d9cbd9211034ddcfb231e8becab5
DesReeAnne <desereeanne.morales.cics@ust.edu.ph>
2025-05-17 21:36:32 +0800

    pacheck css ng footer

--- aerogen/src/assets/Footer.jsx
+++ aerogen/src/assets/Footer.jsx
@@ -5,7 +5,7 @@ function Footer({logo}) {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <img src={logo} alt="Logo" className="footer-logo" />
+        <img src="/logo.png" alt="Logo" className="logo-image" />
         <small className="footer-text">© {new Date().getFullYear()} Aerogen. All rights reserved.</small>
       </div>
     </footer>
--- aerogen/src/assets/NavBar.jsx
+++ aerogen/src/assets/NavBar.jsx
@@ -7,12 +7,16 @@ function NavBar() {
       <div className="navbar-container">
         <div className="logo">
           <img src="/logo.png" alt="Logo" className="logo-image" />
+          <div className="logo-text">
+            <span className="english">AEROGEN</span>
+            <span className="japanese">エアロゲン</span>
+          </div>
         </div>
         <ul className="nav-list">
-             <li className="nav-item"><Link to="/HomePage" className="nav-link">Home</Link></li>
+          <li className="nav-item"><Link to="/HomePage" className="nav-link">Home</Link></li>
           <li className="nav-item"><Link to="/Aircrafts" className="nav-link">Our Aircrafts</Link></li>
-          <li className="nav-item"><Link to="/ServicesPage" className="nav-link"> Services</Link></li>
-           <li className="nav-item"><Link to="/DevelopersPage" className="nav-link">Developers</Link></li>  
+          <li className="nav-item"><Link to="/ServicesPage" className="nav-link">Services</Link></li>
+          <li className="nav-item"><Link to="/DevelopersPage" className="nav-link">Developers</Link></li>  
           <li className="nav-item"><Link to="/loginPage" className="nav-link">Login</Link></li> 
           <li className="nav-item"><Link to="/ContactPage" className="nav-link">Contact Us</Link></li> 
         </ul>
